01db108eed7073da7ef863a88f2388549986513e
adrianHenrique60 <141146285+adrianHenrique60@users.noreply.github.com>
2024-07-15 14:14:35 -0300

    feat: add config server

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Especifica que o servidor deve escutar em todas as interfaces de rede
+    port: 3000,       // Porta que você quer que o servidor escute
+  }
 })
